1f79424a2bf6c538eed8a6d6c24d59d3e7e0523f
Marks Duarte <marksduarte@gmail.com>
2023-06-18 00:04:57 -0300

    Update SealedClass.java

--- src/main/java/dev/marksduarte/sealed_class/SealedClass.java
+++ src/main/java/dev/marksduarte/sealed_class/SealedClass.java
@@ -2,7 +2,7 @@ package dev.marksduarte.sealed_class;
 
 public sealed class SealedClass permits FinalClass, NonSealedClass, SealedClassB {
     // Classes seladas devem definir quais classes podem herdá-la.
-    // A classe sealed e as classes que devem implementa-lá, devem estar
+    // A classe sealed e as classes que devem implementá-la, devem estar
     // no mesmo pacote.
 
     // As classes que herdarem devem ter alguns desse modificadores: final, sealed ou non-sealed.
